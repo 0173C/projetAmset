b269cbe1050a437ee57fba5d1f52e1d458572f41
AaronSALL <125438136+AaronSALL@users.noreply.github.com>
2023-11-15 16:52:21 +0100

    login

---
+++ login.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+ <meta charset="utf-8">
+ <link rel="stylesheet" href="style.css">
+ </head>
+ 
+ <body>
+ <div id="container">
+
+ <form action="verification.php" method="POST">
+ <h1>Login</h1>
+
+ <label><b>Nom de l'utilisateur</b></label>
+ <input type="text" placeholder="Nom" name="username" required>
+
+ <label><b>Mot de passe</b></label>
+ <input type="password" placeholder="Mot de passe" name="password" required>
+
+ <input type="submit" id='submit' value='LOGIN'>
+ <?php
+ if(isset($_GET['erreur'])){
+ $err = $_GET['erreur'];
+ if($err==1 || $err=2)
+ echo "<p style='color:red'>Utilisateur ou Mot de passe incorrect</p>";
+ }
+ ?>
+ </div>
+ </body>
+</hmtl>
