04f624e654abf7e7a3beaf497ee9cb7307bbdde6
Hadrien14 <128477370+Hadrien14@users.noreply.github.com>
2023-12-06 15:30:16 +0100

    update feuille

--- feuille_salaries.php
+++ feuille_salaries.php
@@ -2,7 +2,7 @@
 include("index.php");
 echo '<link rel="stylesheet" href="./cv.css">';
 header('content-type: text/html; charset=utf-8');
-include("connexion.php"); // connexion BDD 
+include("connexion.php"); 
 
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
@@ -10,21 +10,26 @@ if (isset($_GET["id"])) {
     echo "Erreur";
 }
 
-$salarie = "SELECT * FROM fiche_salarie.salarie WHERE idSalarie = $id ";
+$salarie = "SELECT s.nomSalarie, s.prenomSalarie, s.site, s.competences, st.nomSite 
+            FROM fiche_salarie.salarie s
+            LEFT JOIN fiche_salarie.sites st ON s.site = st.idSite";
+
 $result = $conn->query($salarie);
 
 echo "<button id='disconnect'>Se déconnecter</button>";
 
-$previousValue = null;
+echo '<body><div id="content">'; 
 
-echo '<body><div id=content>';
-while ($row = $result->fetch_array()) {
-    foreach ($row as $key => $value) {
-        if ($value !== $previousValue) {
-            echo '<div id="' . $key . '">' . $value . '</div>';
-            $previousValue = $value;
-        }
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_array()) {
+        echo '<div id="nomSalarie">' . $row['nomSalarie'] . '</div>';
+        echo '<div id="prenomSalarie">' . $row['prenomSalarie'] . '</div>';
+        echo '<div id="nomSite">' . $row['nomSite'] . '</div>';
+        echo '<div id="competences">' . $row['competences'] . '</div>';
     }
+} else {
+    echo "Aucun résultat trouvé.";
 }
-echo '</div></body>';
+
+echo '</div></body>'; 
 ?>
