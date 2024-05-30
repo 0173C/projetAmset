90c7381d442299d8ae42d2dde04e422e23b839c1
Hadrien14 <128477370+Hadrien14@users.noreply.github.com>
2024-05-30 14:07:57 +0200

    Update verification.php

--- verification.php
+++ verification.php
@@ -2,7 +2,7 @@
 session_start();
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $db_username = 'root';
-    $db_password = '';
+    $db_password = 'N-Word';
     $db_name = 'fiche_salarie';
     $db_host = 'localhost';
     $db = mysqli_connect($db_host, $db_username, $db_password, $db_name)
